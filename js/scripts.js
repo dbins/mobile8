00825dab7cb9cd18cfcf2173fa99c1aaacebf077
dbins <bins@ig.com.br>
2015-11-07 20:43:11 -0300

    2015-11-07

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+		//http://www.javascriptlint.com/online_lint.php
+		
 		//Classe para emular o objeto de geolocalizacao
 		function Position(latitude, longitude, altitude, accurary, altitudeAccuracy, heading, speed) {
 			this.coords = function(latitude, longitude, altitude, accurary, altitudeAccuracy, heading, speed) {
@@ -8,7 +10,7 @@
 				this.altitudeAccuracy = altitudeAccuracy;
 				this.heading = heading;
 				this.speed = speed;
-			}
+			};
 		}
 		
 		var Position2 = {
@@ -21,54 +23,56 @@
 				heading : "",
 				speed : ""
 			}
-		}
+		};
 		
 		function echeck(str) {
-			var at="@"
-			var dot="."
-			var lat=str.indexOf(at)
-			var lstr=str.length
-			var ldot=str.indexOf(dot)
+			var at="@";
+			var dot=".";
+			var lat=str.indexOf(at);
+			var lstr=str.length;
+			var ldot=str.indexOf(dot);
 			if (str.indexOf(at)==-1){
 			   //alert("Invalid E-mail ID")
-			   return false
+			   return false;
 			}
 
 			if (str.indexOf(at)==-1 || str.indexOf(at)==0 || str.indexOf(at)==lstr){
 			   //alert("Invalid E-mail ID")
-			   return false
+			   return false;
 			}
 
 			if (str.indexOf(dot)==-1 || str.indexOf(dot)==0 || str.indexOf(dot)==lstr){
 				//alert("Invalid E-mail ID")
-				return false
+				return false;
 			}
 
 			 if (str.indexOf(at,(lat+1))!=-1){
 				//alert("Invalid E-mail ID")
-				return false
+				return false;
 			 }
 
 			 if (str.substring(lat-1,lat)==dot || str.substring(lat+1,lat+2)==dot){
 				//alert("Invalid E-mail ID")
-				return false
+				return false;
 			 }
 
 			 if (str.indexOf(dot,(lat+2))==-1){
 				//alert("Invalid E-mail ID")
-				return false
+				return false;
 			 }
 			
 			 if (str.indexOf(" ")!=-1){
 				//alert("Invalid E-mail ID")
-				return false
+				return false;
 			 }
 
-			 return true					
+			 return true;				
 		}
 		
 		function rand(min,max,interval)	{
-			if (typeof(interval)==='undefined') interval = 1;
+			if (typeof(interval)==='undefined') {
+			interval = 1;
+			}
 			var r = Math.floor(Math.random()*(max-min+interval)/interval);
 			return r*interval+min;
 		}
